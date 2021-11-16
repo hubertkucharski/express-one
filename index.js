d80f600b1e8845893d5130047e19b3935a94a501
hubertkucharski <hubertkucharski@gmail.com>
2021-11-16 21:37:08 +0100

    only client.json

--- index.js
+++ index.js
@@ -1,35 +1,36 @@
 const express = require('express');
-const hbs = require('express-handlebars');
-const methodOverride = require('method-override');
-const { clientRouter } = require('./routers/client');
-const { homeRouter } = require('./routers/home');
-// const { db } = require('./utils/db');
-const { handleError } = require('./utils/errors');
+const product = require('./api/product');
 
+// const cookieParser = require('cookie-parser');
+//
 const app = express();
-
-app.use(methodOverride('_method'));
-app.use(express.urlencoded({
-  extended: true,
-}));
-app.use(express.static('public'));
-app.use(express.json());
-app.engine('.hbs', hbs({
-  extname: '.hbs',
-  // helpers: handlebarsHelpers
-}));
-app.set('view engine', '.hbs');
-// wersja bez routera używa get
-// app.get('/', (req, res) => {
-//     res.render('test')
+// app.use(cookieParser());
+app.use('/api/product', product);
+app.use('/', (req, res) => {
+  res.send('Home site');
+});
+// const object1 = {
+//   a: 'somestring',
+//   b: 42,
+// };
+//
+// const entryObject = Object.entries(object1);
+//
+// for (const [one, sec] of entryObject) {
+//   console.log(one, sec);
 // }
-// wersja z router musi byc z use
-app.use('/', homeRouter);
-app.use('/client', clientRouter);
-app.get('/test', (req, res) => {
-  res.send('Ok');
-});
-app.use(handleError);
-app.listen(3000, '0.0.0.0', () => {
-  console.log('running server on http:/localhost:3000');
-});
+// app.get('/', (req, res) => {
+//   console.log(req.originalUrl);
+//   res
+//     .cookie('ciastko1', 'niamniam')
+//     .send(req.cookies);
+// });
+//
+app.listen('3000', () => { console.log('server is running..'); });
+// const myPromise = new Promise();
+// (async () => {
+//   await setTimeout(() => {
+//     console.log('koniec czasu');
+//   }, 1000);
+//   await setInterval(() => console.log('500'), 500);
+// })();
